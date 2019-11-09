3cbd666fcd79aed60749cf128789c0003a52fdcc
Sean Good <tree662@protonmail.com>
2019-11-09 20:33:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
+katz_deli = []
+
+def line(array)
+    if array.length == 0
+      puts "The line is currently empty."
+    else
+     array.each_with_index do |line, other_deli|
+      puts "The line is currently: #{line} #{other_deli}"
+    
+  end
+end 
+  
+  
+
 # Write your code here.
